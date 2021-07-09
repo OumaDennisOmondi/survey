3d6e08a12562599a9dcd08bdf1220c5b8d6cd2ae
Dennis Omondi <oumadennisomondi@gmail.com>
2021-07-09 08:52:02 +0300

    Fixed anchor tags

--- src/Login.js
+++ src/Login.js
@@ -53,9 +53,7 @@ function Login(props) {
 
         <div className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
             <header className="max-w-lg mx-auto">
-                <a href="#">
                     <h1 className="text-4xl font-bold text-white text-center">Startup</h1>
-                </a>
             </header>
 
             <main className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
@@ -83,15 +81,6 @@ function Login(props) {
                 </section>
             </main>
 
-            <div className="max-w-lg mx-auto text-center mt-12 mb-6">
-                <p className="text-white">Don't have an account? <a href="#" className="font-bold hover:underline">Sign up</a>.</p>
-            </div>
-
-            <footer className="max-w-lg mx-auto flex justify-center text-white">
-                <a href="#" className="hover:underline">Contact</a>
-                <span className="mx-3">•</span>
-                <a href="#" className="hover:underline">Privacy</a>
-            </footer>
         </div>
         </>
     )
